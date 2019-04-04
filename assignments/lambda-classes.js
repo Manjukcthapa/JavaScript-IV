133d32de7b9d522dec3693084f91bd081adb0fe6
Manju KC <Manju KC>
2019-04-04 17:30:30 -0400

    complete the lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -39,4 +39,119 @@ const Jonny = new Person({
 // grade receives a student object and a subject string as arguments 
 // and logs out '{student.name} receives a perfect score on {subject}'
 
-class 
+class Instructor extends Person{
+    constructor(object){
+        super(object);
+        this.newSpecialty = object.speciality;
+        this.newFavLanguage = object.favLanguage;
+        this.newCatchPhrase = object.CatchPhrase
+    }
+    demo(subject){
+        return ` we are learning about ${subject}`
+    }
+
+    grade(subject){
+        return `${this.newName} receives a perfect score on ${subject}`
+
+    }
+
+
+}
+
+let josh = new  Instructor ({
+  name: "Josh Knell",
+  location: "California",
+  age:35,
+  specialty:  "redux",
+  favLanguage :"JavaScript",
+  catchPhrase : "Don't forget the homies"
+});
+
+console.log(josh.demo("ruby"));
+console.log(josh.grade("Java"));
+
+
+// Student
+// Now we need some students!
+// Student uses the same attributes that have been set up by Person
+// Student has the following unique props:
+// previousBackground i.e. what the Student used to do before Lambda School
+// className i.e. CS132
+// favSubjects. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+// Student has the following methods:
+// listsSubjects a method that logs out all of the 
+// student's favoriteSubjects one by one.
+// PRAssignment a method that receives a subject as an argument and logs out
+//  that the student.name has submitted a PR for {subject}
+// sprintChallenge similar to PRAssignment but logs out student.name has begun 
+// sprint challenge on {subject}
+
+class Student extends Person{
+    constructor(object){
+        super(object);
+        this.newpreviousBackground  = object.previousBackground ;
+        this.newclassName = object.className;
+        this.newfavSubjects = object.favSubjects;
+    }
+    
+    PRAssignment(subject){
+       return `${this.newName} has submitted a PR for ${subject}` 
+    }
+
+}
+
+let Manju = new  Student ({
+  name: "Manju",
+  location: "Atlanta",
+  age:20,
+  previousBackground :  "Cashier",
+  className :"Web19",
+  favSubjects : "['Html', 'CSS', 'JavaScript']"
+});
+
+console.log(Manju. PRAssignment("java"));
+console.log(Manju. PRAssignment("Html"));
+console.log(Manju. PRAssignment("Javascript"));
+
+
+// Project Manager
+// Now that we have instructors and students, we'd be nowhere without our PM's
+// ProjectManagers are extensions of Instructors
+// ProjectManagers have the following unique props:
+// gradClassName: i.e. CS1
+// favInstructor: i.e. Sean
+// ProjectManagers have the following Methods:
+// standUp a method that takes in a slack channel and 
+// logs `{name} announces to {channel}, @channel standy times!​​​​​
+// debugsCode a method that takes in a student object and a subject and 
+// logs out {name} debugs {student.name}'s code on {subject}
+
+class ProjectManagers extends Instructor{
+    constructor(object){
+        super(object);
+        this.newgradClassName  = object.gradClassName;
+        this. newfavInstructor = object. favInstructor;
+    }
+    
+StandUp(channel){
+   return `${this.newName} announces to ${channel}, @channel standy times!​​​​​`
+}
+debugsCode(studentName,subject){
+return `${this.newName} debugs ${studentName}'s code on ${subject}`;
+}
+}
+
+let Bradly = new  ProjectManagers ({
+  name: "Bradly",
+  location: "Russia",
+  age:24,
+  specialty:  "redux",
+  favLanguage :"JavaScript",
+  catchPhrase : "Don't forget the homies",
+  gradClassName:" CS1",
+  favInstructor: " Sean"
+
+});
+
+console.log(Bradly. StandUp("web19_Bradley"));
+console.log(Bradly. debugsCode("manju","Javascript"));
